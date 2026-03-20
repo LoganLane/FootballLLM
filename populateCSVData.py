ec992cc9f7351f5e2f7b1645db34b459e8127547
Logan Lane <34287344+LoganLane@users.noreply.github.com>
2026-03-20 15:35:38 -0400

    Update CSV path placeholder in populateCSVData.py

--- populateCSVData.py
+++ populateCSVData.py
@@ -1,7 +1,7 @@
 import duckdb
 
 conn = duckdb.connect('my_database.duckdb')
-path = '/home/logantl/Desktop/DataToBeUsed/2025/'
+path = 'Your path to NFL Big Data Bowl 2025 CSV folder here...'
 conn.sql(f"CREATE TABLE players AS SELECT * FROM read_csv_auto('{path}/players.csv')")
 conn.sql(f"CREATE TABLE games AS SELECT * FROM read_csv_auto('{path}/games.csv')")
 conn.sql(f"CREATE TABLE plays AS SELECT * FROM read_csv_auto('{path}/plays.csv')")
